0abaa3028d8343f025bbf898857c9cd475548ddf
Tubaleviao <alvaro.prata.filho@gmail.com>
2020-06-13 10:28:20 -0400

    removing unnused first embedded project

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
+import Player from './player'
 
-let bookList = [
-	{"title": "Hunger", "author": "Roxane Gay", "pages": 320},
-	{"title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260},
-	{"title": "White Teeth", "author": "Zadie Smith", "pages": 480},
-	{"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304}
-]
-
-const Library = (books) => books.map(b => <div>{b.title}</div>)
+let songs = [{title:"ho"},
+			{title:"ha"},
+			{title:"he"},
+			{title:"hi"}]
 
 render(
-  <Library books={bookList} />, 
+  <Player songs={songs} />,
   document.getElementById('root')
 );
